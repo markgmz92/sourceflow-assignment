0b8634b6c4f387ddb5595ad355dbf7923aad0b35
markgmz92 <markgmz92@gmail.com>
2023-07-25 14:39:44 +0800

    Boostrap Installed

--- app/page.js
+++ app/page.js
@@ -1,6 +1,9 @@
 import Image from 'next/image';
-import styles from './page.module.css';
 
 export default function Home() {
-  return <main className={styles.main}>Hello World</main>;
+  return (
+    <main>
+      <h1>Hello World</h1>
+    </main>
+  );
 }
